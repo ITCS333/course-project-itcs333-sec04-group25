8b5d026493c141f04464aa251e6b3924e16d80a1
NOT-LT <t.aljamri55@gmail.com>
2025-11-25 17:42:49 +0300

    Added Databasehelper.php to create Database connection through PDO.

---
+++ src/common/DBConfig.php
@@ -0,0 +1,13 @@
+<?php
+// Database configuration
+$config = [
+    'host' => 'localhost',
+    'dbname' => 'course',
+    'username' => 'admin',
+    'password' => 'password123',
+    '`options`' => [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]
+];
